5066843a87f3914f3870fc7811ed783e76161f73
Chinedum Emmanuel <gilbertemmanuel45@gmail.com>
2026-03-31 09:39:51 -0700

    updated some unchecked ids

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import auth from './middleware/auth.js'; 
-import User from './models/User.js';
 import Task from './models/Task.js';
 
 const app = express()
@@ -27,67 +23,33 @@ mongoose.connect(process.env.MONGO_URI)
 // Define API endpoints
 // Create a new task
 app.post("/tasks", async (req, res) => {
-    const newTask = new Task(req.body)
-    await newTask.save()
-    res.json(newTask)
-})
-
-app.post("/register", async (req, res) => {
-    try{
-        const { email, password} = req.body
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const user = new User({
-            email,
-            password: hashedPassword
-        })
-        await user.save()
-
-        res.json({
-            message: "User registered successfully"  })
-
-    } catch (err) {
-        res.status(500).json({ error: err.message})
-    }
-})
-
-app.post("/login", async (req, res) => {
     try {
-        const { email, password} = req.body
-        const user = await User.findOne({ email})
-
-        if (!user) {
-            return res.status(400).json({ message: "User not found"})
-
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password)
-
-        if (!isMatch) {
-            return res.status(400).json({ message: "Invalid credentials"})
-        }
-
-        const token = jwt.sign(
-            { userId: user._id },
-            "secretkey",
-            { expiresIn: "1d"}
-        )
-
-        res.json({ token})
+        const newTask = new Task(req.body)
+        await newTask.save()
+        res.status(201).json(newTask)
     } catch (err) {
-        res.status(500).json({ error: err.message})
+        res.status(400).json({ error: err.message })
     }
-    })
+})
 
 // Get all tasks
 app.get("/tasks", async (req, res) => {
-    const tasks = await Task.find()
-    res.json(tasks)
+    try {
+        const tasks = await Task.find()
+        res.json(tasks)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 })
 
 // Delete a task
 app.delete("/tasks/:id", async (req, res) => {
-    await Task.findByIdAndDelete(req.params.id)
-    res.json({ message: "Deleted permanently" })
+    try {
+        await Task.findByIdAndDelete(req.params.id)
+        res.json({ message: "Deleted permanently" })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 })
 // Update a task
 app.put("/tasks/:id", async (req, res) => {
